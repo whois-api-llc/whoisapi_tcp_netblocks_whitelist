860e52fc84814fdf3f0e27f3fad682bb072b7204
Matyas Koniorczyk <matyas@whoisxmlapi.com>
2022-04-05 05:26:43 -0400

    fixed shell script

--- firewall_tcp_update_iptables.sh
+++ firewall_tcp_update_iptables.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-"""
-BASH script to add additional firewall rules for inbound tcp,
-based on a file with allowed IPs and CIDRs.
-Usable example code. 
-Details in the README file
-(c) WhoisXML API, Inc. 2022.
-Licensed under GPL.
-"""
+
+#based on a file with allowed IPs and CIDRs.
+#Usable example code. 
+#Details in the README file
+#(c) WhoisXML API, Inc. 2022.
+#Licensed under GPL.
+
 
 #Customize here
 #SSH port.
@@ -21,6 +20,7 @@ if [ ! -f $WHITELISTFILE ];then
    echo "Error: $WHITELISTFILE does not exist."
    echo "Create one with a default whitelisted IP or range"
    exit 1
+fi
 
 #Echo the configuration
 echo "ssh port: $SSHPORT"
